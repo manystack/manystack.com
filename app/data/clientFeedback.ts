5f93884f2a330bda16010fc2b4488bab86f810a4
Szabolcs Markó <akksi@akksi.eu>
2023-10-09 20:56:32 +0200

    Display Client Feedback (#1)
    * Add some real client feedback data
    
    * Render client feedbacks on homepage
    
    * Display Client Feedback as cards in a scrollable sidebar

---
+++ app/data/clientFeedback.ts
@@ -0,0 +1,156 @@
+import {Feedback} from "@/app/types/types";
+
+export const clientFeedback: Feedback[] = [
+    {
+        id: "21b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "Szabolcs didn’t have the experience I was looking for with this job, but I never regretted hiring him. He quickly understood what I was looking to accomplish and not only implemented it but also made great suggestions and improvements. I am only canceling the job because I am at a point where I need to revise the whole thing. I would hire Szabolcs again if we have more work like this.",
+        client: {
+            name: "Advanced Ads",
+            "website": "https://wpadvancedads.com/"
+        },
+        source: "Upwork",
+        project: {
+            title: "Extend PHP based crawler and reporting tool",
+            term: {
+                from: 2017,
+                to: 2018
+            }
+        }
+    },
+    {
+        id: "a1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "Szabolcs is a GREAT freelancer. We will be hiring him for another project. What is particular about him is his speed, quality, and initiative to make things better. Highly recommended.",
+        client: {
+            name: "Influencers.ae",
+        },
+        source: "Upwork",
+        project: {
+            title: "Instagram Data Crawler",
+            term: {
+                from: 2017,
+                to: 2018
+            }
+        }
+    },
+    {
+        id: "b1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "An Absolute superstar! Will be working again with him shortly!",
+        client: {
+            name: "Influencers.ae"
+        },
+        source: "Upwork",
+        project: {
+            title: "Influencers.ae",
+            term: {
+                from: 2018,
+                to: 2018
+            }
+        }
+    },
+    {
+        id: "c1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "I've worked with Marko for years now, he's exceptional and I recommend him highly to anyone. He will bring solutions to a project, and be a integral part of any team.",
+        client: {
+            name: "Recruiterly"
+        },
+        source: "Upwork",
+        project: {
+            title: "Recruiterly",
+            term: {
+                from: 2018,
+                to: 2021
+            }
+        }
+    },
+    {
+        id: "d1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "Working with Markó great. He is very easy to work with, excellent communication and key the finished project was achieved. 100% recommend and would work with Markó again.\n\n",
+        client: {
+            name: "My Travel Insights"
+        },
+        source: "Upwork",
+        project: {
+            title: "Instagram Insights",
+            term: {
+                from: 2019,
+                to: 2019
+            }
+        }
+    },
+    {
+        id: "e1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "As always. Excellent. I think of Marko as my right hand man, not just for his work but also his thought process, and willingness to not just follow - but come up with ideas and solutions.",
+        client: {
+            name: "Matthew Gibbs - Recruiterly",
+        },
+        source: "Upwork",
+        project: {
+            title: "Recruiterly",
+            term: {
+                from: 0,
+                to: 0
+            }
+        }
+    },
+    {
+        id: "f1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "Easily adjusted to our lead developer. Easy and great to work with. Delivers on time. Highly recommend.",
+        client: {
+            name: "Orsolya Cypert",
+        },
+        source: "Upwork",
+        project: {
+            title: "Orsolya Cypert",
+            term: {
+                from: 2019,
+                to: 2019
+            }
+        }
+    },
+    {
+        id: "g1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "An Absolute rockstar and a pleasure to work with. Hire with confidence.",
+        client: {
+            name: "Influencers.ae",
+        },
+        source: "Upwork",
+        project: {
+            title: "Influencers.ae",
+            term: {
+                from: 2018,
+                to: 2020,
+            }
+        }
+    },
+    {
+        id: "h1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "Exceptional. Very creative, and very talented. The only developer I work with. What's even better is his network of developers he can bring into a project.",
+        client: {
+            name: "Matthew Gibbs -Recruiterly"
+        },
+        source: "Upwork",
+        project: {
+            title: "Recruiterly",
+            term: {
+                from: 2019,
+                to: 2021,
+            }
+        }
+    },
+    {
+        id: "i1b5a023-d889-4bd5-92e3-00ab73f0a5bf",
+        text: "It was an absolute pleasure working with Marko - very professional, committed and creative with finding solutions! Would love to engage Marko for more assignments in the future!",
+        client: {
+            name: "Loylogic",
+            website: "https://www.loylogic.com/",
+        },
+        source: "Upwork",
+        project: {
+            title: "Facebook Messenger Checkbox Plugin & Sponsored Messaging PoC",
+            term: {
+                from: 2020,
+                to: 2020,
+            },
+        }
+    }
+]
