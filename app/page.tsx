54a39bf134bfd334fcb74ce99dd2c8f79cc7bfa7
Szabolcs Markó <akksi@akksi.eu>
2023-10-09 13:56:06 +0200

    Render client feedbacks on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import {clientFeedbacks} from "@/app/data/clientFeedbacks";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg">
       <h1 className="relative flex place-items-center">manystack</h1>
       <p className="text-center text-gray-500">Web and Mobile Application development agency</p>
+        <ul>
+            {clientFeedbacks.map(({id, text, client: {name}}) => (
+                <li key={id}>
+                    {text} - {name}
+                </li>
+            ))}
+        </ul>
     </main>
   )
 }
