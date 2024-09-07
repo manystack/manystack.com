52b78fa6e2e5483a354aac799d924cb53c5a1e5c
Szabolcs Markó <akksi@akksi.eu>
2024-09-07 21:52:04 +0200

    Add project page

---
+++ app/projects/layout.tsx
@@ -0,0 +1,12 @@
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div className="bg-gray-100 p-4 text-center">
+        <h1 className="text-4xl">manystack / projects</h1>
+      </div>
+      <div className="max-w-4xl m-auto">{children}</div>
+    </>
+  )
+}
+
+export default Layout
