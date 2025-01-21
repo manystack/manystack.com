bdd71fa3615f032a5068e5729bde067eb1d34294
Szabolcs Markó <akksi@akksi.eu>
2025-01-21 20:57:06 +0100

    Format Markdown pages

--- app/areas-of-expertise/layout.tsx
+++ app/areas-of-expertise/layout.tsx
@@ -5,7 +5,10 @@ type Props = {
   children: ReactNode
 }
 
-const Layout = ({ children }: Props) => <PagesLayout title="areas of expertise">{children}
+const Layout = ({ children }: Props) => <PagesLayout title="areas of expertise">
+  <div className="prose prose-a:text-blue-500 prose-ul:list-disc prose-ol:list-decimal">
+    {children}
+  </div>
 </PagesLayout>
 
 export default Layout
